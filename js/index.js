373d51093c615eda81011192c40d6b12b752b058
G1ory <1015172612@qq.com>
2017-03-06 18:54:49 +0800

    18.54

--- js/index.js
+++ js/index.js
@@ -113,72 +113,7 @@ $(window).ready(function() {
 	});
 
 	/********************List切换********************/
-	//	$(".item-r div").eq(0).show().siblings("div").hide();
-	//	console.log($(".item-r").length);
-	//	var Arr = jQuery.makeArray($(".item-r div"));
-	//	console.log(Arr);
-	//	for(var j = 0; j < Arr.length; j++) {
-	//		$(".item-r div").eq(j).show().siblings("div").hide();
-	//	}
-//	var divs = $(".item");
-//	var divArr = jQuery.makeArray(divs);
-//
-//		console.log(divArr);
-////	console.log(divs);
-//
-//	for(var i = 0; i < divs.length; i++) {
-
-		//		var str = divs[i].className.substr(0, 9);
-		//		console.log(str);
-
-
-
-//		$(+divs[i] + " .item-r" + " div").eq(0).show().siblings("div").hide();
-		//		console.log($("." + str + " .item-r" + " div").eq(0));
-		//		listTab(str);
-		//			console.log(str);
-//	}
-
-	//	console.log($(".item .shop-list"));
-	//
-	//		var divs = $(".item-r");
-	//		//将JQ数组转换成js数组
-	//		var divArr = jQuery.makeArray(divs);
-	//		console.log(divArr);
-	//		console.log(divs.length);
-	//		//	console.logo($( "+"divs"+" "+"div"));
-	//		//	console.log(" "+divs+" " + "div");
-	//	
-	//		for(var i = 0; i < divArr.length; i++) {
-	//			var str = divArr[i].className.substr(0, 6);
-	//			listTab(str);
-	//	
-	//	}
-	//	console.log("-------");
-	//
-	//	var divsa = $(".shop-list");
-	//	//		//将JQ数组转换成js数组
-	//	var divArrs = jQuery.makeArray(divsa);
-
-	//	for(var i = 0; i < divArrs.length; i++) {
-	//		var strd = divArrs[i].className.substr(0, 9);
-	//		listTab(strd);
-	//
-	//		console.log(strd);
-
-	//		console.log(this);
-	//		console.log(divArr[i].className);
-	//		var str = (divArr[i]).substr(0, 11);
-	//		console.log(str);
-
-	//	}
-
-	//	$(".item-r div").eq(0).show().siblings("div").hide();
-	//	$(".shop-list li").on("mouseenter", function() {
-	//		$(this).siblings("li").removeClass("current").end().addClass("current");
-	//		$(".item-r div").eq($(this).index()).show().siblings("div").hide();
-	//	});
-
+	listHide();
 });
 var timer = null;
 var i = 0;
@@ -195,8 +130,8 @@ function showImg() {
 }
 
 function bannerShow(flag) {
-//	var div = document.getElementsByClassName("content-r")[0];
-	var div=document.getElementById("ct-r");
+	//	var div = document.getElementsByClassName("content-r")[0];
+	var div = document.getElementById("ct-r");
 	var imgArr = div.children;
 
 	if(flag) {
@@ -255,17 +190,26 @@ function itemShow(flag, ele) {
 	}
 }
 
-function listTab(ele) {
-	//		console.log(ele);
-	$("." + ele + " " + ".shop-list" + " li").on("mouseenter", function() {
+function listHide() {
+	var Arr = $(".none .item-r");
+	//	var arr = jQuery.makeArray(Arr);
+	//	console.log(Arr);
+	//	console.log(arr);
+	//	console.log(Arr.length);
+	var brr = $(".shop-list");
+	for(var i = 0; i <= Arr.length + 1; i++) {
 
-		console.log($(this));
-		$(this).siblings("li").removeClass("current").end().addClass("current");
-		$("." + ele + " item-r" + " div").eq($(this).index()).show().siblings("div").hide();
-		//				console.log($("." + ele + " " + "div"));
-		console.log($("." + ele + " item-r" + " div").eq($(this).index()));
-	});
+		$(".item-r:eq(" + i + ") div").eq(0).show().siblings("div").hide();
 
+		$(".shop-list:eq(" + i + ")").find("li").on("mouseenter", function() {
+			
+			//获取元素在数组中的下标
+//			console.log(brr.index($(this).parent().parent()));
+			var index=brr.index($(this).parent().parent());
+			$(this).siblings("li").removeClass("current").end().addClass("current");
+			$(".none  .item-r:eq("+index+") div").eq($(this).index()).show().siblings("div").hide();
+
+		});
+
+	}
 }
-
-
